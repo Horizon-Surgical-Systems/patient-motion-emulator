f84761588c3f66bdae41da5261532e7174b9314d
Kevin Lai <klai@horizonsurgicalsystems.com>
2026-04-24 14:20:25 -0700

    feat: initial commit for robot trajectory generation

---
+++ TrajectoryPlanner.py
@@ -0,0 +1,38 @@
+import numpy as np
+import math
+    
+class TrajectoryPlanner:
+    def __init__(self, start_point, end_point, max_velocity, max_acceleration):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.max_velocity = max_velocity
+        self.max_acceleration = max_acceleration
+
+    def calculate_ux_trajectory(self) -> np.ndarray:
+        print("Calculating ux trajectory...")
+        return np.array([])
+
+    def calculate_uy_trajectory(self) -> np.ndarray:
+        print("Calculating uy trajectory...")
+        return np.array([])
+        
+    def calculate_uz_trajectory(self) -> np.ndarray:
+        print("Calculating uz trajectory...")
+        return np.array([])
+
+    def calculate_x_trajectory(self) -> np.ndarray:
+        print("Calculating x trajectory...")
+        return np.array([])
+
+    def calculate_y_trajectory(self) -> np.ndarray:
+        print("Calculating y trajectory...")
+        return np.array([])
+
+    def calculate_z_trajectory(self) -> np.ndarray:
+        print("Calculating z trajectory...")
+        return np.array([])
+
+    def calculate_linear_trajectory(self) -> np.ndarray:
+        print("Calculating arbitrary linear trajectory...")
+
+        return np.array([])
---
+++ TrajectoryUtils.py
@@ -0,0 +1,13 @@
+import numpy as np
+import math
+
+class TrajectoryUtils:
+    @staticmethod
+    def trajectory_smoothing(trajectory: np.ndarray) -> np.ndarray:
+        print("Smooth trajectory...")
+        return trajectory
+
+    @staticmethod
+    def trajectory_padding(trajectory: np.ndarray) -> np.ndarray:
+        print("Padding trajectory...")
+        return trajectory
